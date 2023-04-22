aeb0a4522645d358004c117d468166fcfcdfc4e7
luke777 <luke.lindsay@gmail.com>
2023-04-22 22:45:08 +0100

    refactoring

--- md/lindsay2018.py
+++ md/lindsay2018.py
@@ -15,15 +15,7 @@ class Lindsay2018(PaymentRule):
         self.max_perms_to_consider = max_perms_to_consider
         self.seed = seed
 
-    def calc_surplus_shares(self, sol: Solution):
-
-        #Used to give progress updates for slow problems.
-        start_time = time.time()
-        last_time = start_time
-
-        winners = sol.winner_indexes()
-        losers = sol.loser_indexes()
-
+    def gen_permutations(self, winners):
         permutations = []
         rnd = Random(self.seed)
         if math.factorial(len(winners)) > self.max_perms_to_generate:
@@ -38,15 +30,25 @@ class Lindsay2018(PaymentRule):
             rnd.shuffle(permutations)
             permutations = permutations[0:self.max_perms_to_consider]
 
+        return permutations
+
+    def calc_surplus_shares(self, sol: Solution):
+
+        #  Used to give progress updates for slow problems.
+        start_time = time.time()
+        last_time = start_time
+
+        winners = sol.winner_indexes()
+
+
+        permutations = self.gen_permutations(winners)
+        n_perms = len(permutations)
+
+        surplus_calculator = SurplusCalculator(sol)
+
         # Stores running sum of shares for each winner.
         shares_sums = defaultdict(lambda: 0)
 
-        # Cache surplus values to avoid repeatedly solving the same MIP problem.
-        cache = {frozenset(winners): sol.surplus}
-
-        problem = copy(sol.problem)
-        auction = Auction()
-        n_perms = len(permutations)
 
         for j, perm in enumerate(permutations):
             current_time = time.time()
@@ -54,16 +56,10 @@ class Lindsay2018(PaymentRule):
                 last_time = current_time
                 print('Permutation {} of {}'.format(j, n_perms))
             last_surplus = 0
-            problem.bidders = [sol.problem.bidders[i] for i in losers]
-            for position, i in enumerate(perm):
-                problem.bidders.append(sol.problem.bidders[i])
 
-                key = frozenset(perm[0:position + 1])
-                if key in cache.keys():
-                    surplus = cache[key]
-                else:
-                    surplus = auction.surplus_determination(problem)
-                    cache[key] = surplus
+            for position, i in enumerate(perm):
+
+                surplus = surplus_calculator.calc_surplus(perm[0:position + 1])
 
                 share = surplus - last_surplus
                 shares_sums[i] = shares_sums[i] + share
@@ -78,10 +74,34 @@ class Lindsay2018(PaymentRule):
         current_time = time.time()
         if current_time > start_time + 10:
             n_winners = len(winners)
-            n_bidders = n_winners + len(losers)
+            n_bidders = len(sol.problem.bidders)
             solve_time = round(current_time-start_time)
             print('{} bidders and {} winners.  Calculating prices took {} seconds with {} permutations.'.format(n_bidders, n_winners,solve_time, n_perms))
 
+class SurplusCalculator:
+
+    def __init__(self, sol: Solution):
+        self.solution = sol
+        self.winners = sol.winner_indexes()
+        self.losers = sol.loser_indexes()
+        self.auction = Auction()
+        self.problem = copy(sol.problem)
+        # Cache surplus values to avoid repeatedly solving the same MIP problem.
+        self.cache = {frozenset(self.winners): sol.surplus}
+
+    def calc_surplus(self, subset_winners):
+        key = frozenset(subset_winners)
+        if key in self.cache.keys():
+            return self.cache[key]
+        else:
+            self.problem.bidders = []
+            for i in self.losers:
+                self.problem.bidders.append(self.solution.problem.bidders[i])
+            for i in key:
+                self.problem.bidders.append(self.solution.problem.bidders[i])
+            surplus = self.auction.surplus_determination(self.problem)
+            self.cache[key] = surplus
+            return surplus
 
 def get_rule(rule_name,  max_perms_to_consider: int = 1024):
     rule_name = rule_name.lower().strip()  # ignore case
